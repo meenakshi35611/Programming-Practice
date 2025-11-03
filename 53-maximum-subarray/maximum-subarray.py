12391f8344ea9fefd5959606ff646d46d834e424
meenakshi35611 <meenusarovar@gmail.com>
2025-11-03 11:39:08 +0530

    Refactor maxSubArray for better performance
    Refactor maxSubArray method for improved performance.

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,4 +1,4 @@
-class Solution(object):
+'''class Solution(object):
     def maxSubArray(self, nums):
         current_sum = 0
         max_sum = float('-inf')  
@@ -7,4 +7,17 @@ class Solution(object):
             current_sum = max(num, current_sum + num)
             max_sum = max(max_sum, current_sum)
 
+        return max_sum'''
+
+#Another code i found was better than mine
+class Solution(object):
+    def maxSubArray(self, nums):
+        max_sum=nums[0]
+        current_sum=0
+        for i in nums:
+            if current_sum <0:
+                current_sum=0
+            current_sum+=i
+            if current_sum > max_sum:
+                max_sum =current_sum
         return max_sum
